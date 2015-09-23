7f987e00b8046098c5ca60a8714d634f09025eee
Ablay <atomio.ak@gmail.com>
2015-09-23 07:39:51 +0600

    Get rid from iso library

--- client/app_state.js
+++ client/app_state.js
@@ -1,18 +1,7 @@
-import Iso from 'iso';
 import alt from './alt';
-import vent from './modules/vent';
 import currentUser from './stores/current_user';
 import auth from './modules/auth';
 
 
-Iso.bootstrap((state, meta, container) => {
-  window.titleNode = document.getElementsByTagName('title')[0];
-  window.appNode = container;
-
-  alt.bootstrap(state);
-  vent.trigger('state:initialized');
-
-  if (currentUser.authorized()) {
-    auth.setTokenHeaders(currentUser.get());
-  }
-});
+alt.bootstrap(JSON.stringify(window.app.stores));
+if (currentUser.authorized()) auth.setTokenHeaders(currentUser.get());
