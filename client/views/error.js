766a2c852f9246d987b40c5d74c2c1bc0bcc8fd7
Max Drachsh <sharok89@gmail.com>
2015-09-27 14:28:07 +0600

    Move styles from tags to class

--- client/views/error.js
+++ client/views/error.js
@@ -17,8 +17,8 @@ export default class Error extends Component {
       </a>
       <div className="p-e-top"></div>
       <div className="p-e-description">
-        <h1>{this.lang.messages.error_title}</h1>
-        <p>{this.lang.messages.error_description}</p>
+        <h1 className="p-e-d-title">{this.lang.messages.error_title}</h1>
+        <p className="p-e-d-message">{this.lang.messages.error_description}</p>
       </div>
       <div className="p-e-bottom"></div>
       <Footer />
