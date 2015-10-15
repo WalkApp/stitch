fbbd9e9a81b29e751ca2e2d12919360f9abfd2d9
Ablay <atomio.ak@gmail.com>
2015-10-16 04:58:09 +0600

    Pass calback

--- client/controllers/error.js
+++ client/controllers/error.js
@@ -3,6 +3,6 @@ import Controller from '../base/controller';
 
 export default class ErrorController extends Controller {
   index (ctx, done) {
-    this.renderErrorView({ status: 404 });
+    this.renderErrorView({ status: 404 }, done);
   }
 }
