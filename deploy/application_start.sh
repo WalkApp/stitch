dc863671ad94cc891f7464056b13b9f7df32c46c
Max Drachsh <sharok89@gmail.com>
2015-10-22 15:52:37 +0600

    Fix passing NODE_ENV to forever lib

--- deploy/application_start.sh
+++ deploy/application_start.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-env NODE_ENV=staging
 env APPLICATION_PATH="/home/ubuntu/walk/stitch/index.js"
 
-exec forever start $APPLICATION_PATH
+NODE_ENV=staging forever start $APPLICATION_PATH
--- deploy/application_stop.sh
+++ deploy/application_stop.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 env APPLICATION_PATH="/home/ubuntu/walk/stitch/index.js"
 
-exec forever stop $APPLICATION_PATH
+forever stop $APPLICATION_PATH
