94e6bc466d77320fe41f87e471380aa89c6888fe
Max Drachsh <sharok89@gmail.com>
2015-10-23 11:10:27 +0600

    Test code deploy

--- client/views/welcome.js
+++ client/views/welcome.js
@@ -23,7 +23,7 @@ export default class Welcome extends Component {
             </p>
           </section>
           <section>
-            <h4>TestGet know what happen in your city!</h4>
+            <h4>Get know what happen in your city!</h4>
             <p>
               It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
             </p>
