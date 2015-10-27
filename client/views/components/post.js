525588e3c939e261f1882d8348e202e171e7b2e1
Max Drachsh <sharok89@gmail.com>
2015-10-27 22:36:52 +0600

    Fix jscs errors

--- client/views/components/post.js
+++ client/views/components/post.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import moment from 'moment'
+import moment from 'moment';
 import Component from '../../base/component';
 import currentUser from '../../stores/current_user';
 
