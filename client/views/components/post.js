90c148481ca581fcbb1c5d8606b34ba3b29f8840
Max Drachsh <sharok89@gmail.com>
2015-10-27 22:34:28 +0600

    Post component redesign

--- client/views/components/post.js
+++ client/views/components/post.js
@@ -25,13 +25,28 @@ export default class Post extends Component {
         ? <div className="m-p-body"><img className="c-p-image" src={post.image_urls[0]} /></div>
         : false
       }
-      <div className="c-p-info m-p-note">
-        <i className="icon-pin"></i> {post.address}
+      <div className="c-p-info m-p-body">
+        <div className="c-p-i-icon">
+          <i className="icon-pin"></i>
+        </div>
+        <div className="c-p-i-address">{post.address}</div>
       </div>
-      <div className="m-p-body">
+      <div className="c-p-description">
         <p>
           {post.description}
         </p>
+      </div>
+      <div className="c-p-footer">
+        <div className="c-p-f-likes">
+          <i className="icon-pin"></i>
+          <strong className="c-p-f-l-count">12</strong>
+        </div>
+        <div className="c-p-f-actions">
+          <button type="button" className="m-btn m-btn-sm m-btn-light" data-uploader="post-images">
+            <i className="icon-upcoming"></i>{this.lang.captions.add_event}
+          </button>
+        </div>
+
       </div>
     </div>;
   }
